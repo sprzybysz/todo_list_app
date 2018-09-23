34a9644d8defd4b58d448afbad62401787794c9a
sprzybysz <s.przybysz@blueowl.pl>
2018-09-23 21:33:52 +0200

    nth commit

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import {
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Button
-} from 'reactstrap';
-import {
-  CSSTransition,
-  TransitionGroup
-} from 'react-transition-group';
-import uuid from 'uuid';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class TodoList extends Component {
-
   componentDidMount() {
     this.props.getItems();
   }
 
-  onDeleteClick = (id) => {
+  onDeleteClick = id => {
     this.props.deleteItem(id);
   };
 
@@ -28,46 +18,20 @@ class TodoList extends Component {
     const { items } = this.props.item;
     return (
       <Container>
-
         <ListGroup>
-          <TransitionGroup className="todo-list">
-            {items.map(({ _id, name, subtask }) => (
+          <TransitionGroup className="shopping-list">
+            {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                  <p
-                    className="text-primary"
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={this.onDeleteClick.bind(this, _id)}
                   >
-                    {name}
-                    <span
-                      className=" ml-2"
-                      style={{
-                        cursor: "pointer"
-                      }}
-                      onClick={this.onDeleteClick.bind(this, _id)}
-                    >
-                      &times;
-                                </span>
-
-                  </p>
-                  {/* 
-                                <ul>
-                                    {subtask.map((subItem) => (
-
-                                        <li key={subItem + uuid} className="text-danger">
-                                            {subItem}
-                                        <span
-
-                                        onClick={() => {
-
-                                        }}
-                                        > &times;</span>
-                                    </li>
-                                    ))}
-                                </ul>
-                                
-                                
-                                */}
-
+                    &times;
+                  </Button>
+                  {name}
                 </ListGroupItem>
               </CSSTransition>
             ))}
@@ -78,14 +42,16 @@ class TodoList extends Component {
   }
 }
 
-TodoList.propTypes = {
+ShoppingList.propTypes = {
   getItems: PropTypes.func.isRequired,
   item: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   item: state.item
 });
 
-export default connect(mapStateToProps, { getItems, deleteItem })(TodoList);
-
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem }
+)(TodoList);
