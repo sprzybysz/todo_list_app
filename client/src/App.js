85ce45058d17247b5429103f0df53daa19e9e95c
sprzybysz <s.przybysz@blueowl.pl>
2018-09-23 21:17:24 +0200

    config files modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
+import ShoppingList from './components/TodoList';
 import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
 
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb://YOUR_MONGO_URI'
-};
+    mongoURI: 'mongodb://sprzybysz:sprzybysz123@ds117362.mlab.com:17362/todo-app'
+}
